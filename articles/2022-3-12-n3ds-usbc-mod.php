21c31f31428ebcf412dc02fa8a42f32a22926907
Eric <eric@econrad.org>
2022-12-26 19:49:58 +0100

    website 1.0

---
+++ articles/2022-3-12-n3ds-usbc-mod.php
@@ -0,0 +1,16 @@
+<article class="card mb-4">
+<header class="card-header">
+    <div class="card-meta">
+    <a href="#"><time class="timeago" datetime="2022-03-12 20:00">12 march 2022</time></a> in <a href="page-category.html">Electronics</a>
+    </div>
+    <a href="electronics/new-3ds-usbc-mod.php">
+    <h4 class="card-title">USB-C N3ds</h4>
+    </a>
+</header>
+<a href="electronics/new-3ds-usbc-mod.php">
+    <img class="card-img" src="img/electronics/new-3ds-usbc-mod/1.png" alt="" />
+</a>
+<div class="card-body">
+    <p class="card-text">Modding a New Nintendo 3ds (non-xl, 2015) to charge over USB-C. Powered by a tiny Adifuit breakout board.</p>
+</div>
+</article><!-- /.card -->
