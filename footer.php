55fb422e8fb0e0f61b1d8752b8c2649ae302e3ee
Eric <eric@econrad.org>
2024-03-08 09:47:02 -0400

    fixed footer element spacing

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <footer id="footer" class="bg-body-tertiary text-center" style="padding:20px;">
 	<p> econrad.org </p>
 
-	<a class="nav-link" style="display:inline;" target=”_blank” href="https://www.linkedin.com/in/conrade/">Social</a>
-	<a class="nav-link" style="display:inline;" target=”_blank” href="mailto://contact@econrad.org">Contact</a>
-	<a class="nav-link" style="display:inline;" target=”_blank” href="https://github.com/econoboxed">Github</a>
+	<a class="nav-link p-2" style="display:inline;" target=”_blank” href="https://www.linkedin.com/in/conrade/">Social</a>
+	<a class="nav-link p-2" style="display:inline;" target=”_blank” href="mailto://contact@econrad.org">Contact</a>
+	<a class="nav-link p-2" style="display:inline;" target=”_blank” href="https://github.com/econoboxed">Github</a>
 
 </footer>
